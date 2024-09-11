2849a0fe1cdaeeb72a3af984671400daebe8fe58
LucasNeroneRillo <lneronerillo@gmail.com>
2024-09-11 14:03:32 -0500

    Experiment 2 - Removed unnecessary imports

--- Experiments/Lucas_Rillo/experiment_2/hellopeople/src/main/java/coms309/people/PeopleController.java
+++ Experiments/Lucas_Rillo/experiment_2/hellopeople/src/main/java/coms309/people/PeopleController.java
@@ -6,12 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
