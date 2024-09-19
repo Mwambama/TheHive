267a990bfa733367b3a99aa1159314f8b9a318ea
lvrd_ade <fadahunsiadeife11@gmail.com>
2024-09-18 19:42:09 -0500

    added backend template to main

---
+++ Backend/TheHiveApp/src/main/java/com/example/thehiveapp/TheHiveAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.thehiveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHiveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheHiveAppApplication.class, args);
+	}
+
+}
---
+++ Backend/TheHiveApp/src/test/java/com/example/thehiveapp/TheHiveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thehiveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheHiveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
