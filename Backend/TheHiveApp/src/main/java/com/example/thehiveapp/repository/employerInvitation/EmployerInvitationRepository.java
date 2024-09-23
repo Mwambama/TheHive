6a00eb52e98575c1a6de22124325b3c7a9bd3a58
LucasNeroneRillo <lneronerillo@gmail.com>
2024-09-23 18:19:18 -0500

    16-add-employer-invitation-table - Backend - Added Employer Invitation CRUDL

---
+++ Backend/TheHiveApp/src/main/java/com/example/thehiveapp/repository/employerInvitation/EmployerInvitationRepository.java
@@ -0,0 +1,7 @@
+package com.example.thehiveapp.repository.employerInvitation;
+
+import com.example.thehiveapp.entity.employerInvitation.EmployerInvitation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployerInvitationRepository extends JpaRepository<EmployerInvitation, Long> {
+}
