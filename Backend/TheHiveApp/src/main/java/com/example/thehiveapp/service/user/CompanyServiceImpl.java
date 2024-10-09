f98b5af2ed1d2ed1b006917d128dd3438ec4eede
LucasNeroneRillo <lneronerillo@gmail.com>
2024-10-09 13:37:16 -0500

    11-add-address-to-user - Backend - Allow address changes on user endpoint

--- Backend/TheHiveApp/src/main/java/com/example/thehiveapp/service/user/CompanyServiceImpl.java
+++ Backend/TheHiveApp/src/main/java/com/example/thehiveapp/service/user/CompanyServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.thehiveapp.service.user;
 
 import com.example.thehiveapp.entity.user.Company;
 import com.example.thehiveapp.repository.user.CompanyRepository;
+import com.example.thehiveapp.service.address.AddressService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import java.util.List;
 public class CompanyServiceImpl implements CompanyService {
 
     @Autowired private CompanyRepository companyRepository;
+    @Autowired private AddressService addressService;
 
     public CompanyServiceImpl() {}
 
@@ -29,11 +32,13 @@ public class CompanyServiceImpl implements CompanyService {
         );
     }
 
+    @Transactional
     public Company updateCompany(Company request) {
         Long id = request.getUserId();
         if (!companyRepository.existsById(id)) {
             throw new ResourceNotFoundException("Company not found with id " + id);
         }
+        addressService.updateAddress(request.getAddress());
         return companyRepository.save(request);
     }
 
