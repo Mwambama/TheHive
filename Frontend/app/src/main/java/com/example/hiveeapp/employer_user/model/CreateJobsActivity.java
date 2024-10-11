ba46abe19ed034c990f90b8b1fa1375729b7a204
Alisala Mwamba <mwambama@iastate.edu>
2024-10-11 06:59:21 -0500

    issues with employer

--- Frontend/app/src/main/java/com/example/hiveeapp/employer_user/model/CreateJobsActivity.java
+++ Frontend/app/src/main/java/com/example/hiveeapp/employer_user/model/CreateJobsActivity.java
@@ -119,7 +119,7 @@ public class CreateJobsActivity extends AppCompatActivity {
 
     // Create a new job post
     private void createJobPost(JSONObject jobData) {
-        String url = "https://8c5d8b24-4a9a-4ce2-bf22-1aa5316f76a2.mock.pstmn.io/employer_user/post"; // Replace with your mock server URL
+        String url = "http://coms-3090-063.class.las.iastate.edu:8080/job-posting"; // Replace with your mock server URL
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                 Request.Method.POST,
