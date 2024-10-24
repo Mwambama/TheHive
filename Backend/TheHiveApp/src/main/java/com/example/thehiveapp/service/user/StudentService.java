80905d029f99a0bd5fd95c68517bfc0e7410eb2a
lvrd_ade <fadahunsiadeife11@gmail.com>
2024-10-24 02:25:42 -0500

    implemented resume uploads for student

--- Backend/TheHiveApp/src/main/java/com/example/thehiveapp/service/user/StudentService.java
+++ Backend/TheHiveApp/src/main/java/com/example/thehiveapp/service/user/StudentService.java
@@ -1,6 +1,7 @@
 package com.example.thehiveapp.service.user;
 
 import com.example.thehiveapp.entity.user.Student;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -9,5 +10,7 @@ public interface StudentService {
     Student createStudent(Student request);
     Student getStudentById(Long id);
     Student updateStudent(Student request);
+    void uploadStudentResume(Long id, MultipartFile file);
+    byte[] getStudentResume(Long id);
     void deleteStudent(Long id);
 }
