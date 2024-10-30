2f184d6555db1caa4d6777220c7a13b35494b20a
LucasNeroneRillo <lneronerillo@gmail.com>
2024-10-30 08:43:12 -0500

    41-backend-add-core-chat - Updated employer repository for chat

--- Backend/TheHiveApp/src/main/java/com/example/thehiveapp/repository/user/EmployerRepository.java
+++ Backend/TheHiveApp/src/main/java/com/example/thehiveapp/repository/user/EmployerRepository.java
@@ -4,7 +4,9 @@ import com.example.thehiveapp.entity.user.Employer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployerRepository extends JpaRepository<Employer, Long> {
     List<Employer> findByCompanyId(Long companyId);
+    Optional<Employer> findByUserId(Long userId);
 }
--- Backend/TheHiveApp/src/main/java/com/example/thehiveapp/repository/user/StudentRepository.java
+++ Backend/TheHiveApp/src/main/java/com/example/thehiveapp/repository/user/StudentRepository.java
@@ -3,5 +3,9 @@ package com.example.thehiveapp.repository.user;
 import com.example.thehiveapp.entity.user.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByUserId(Long userId);
+
 }
