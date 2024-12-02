7defb37f247002554c691e78fb60031e5aabeaf6
LucasNeroneRillo <lneronerillo@gmail.com>
2024-12-02 17:33:08 -0600

    72-backend-testing-lucas-rillo - Created base test

---
+++ Backend/TheHiveApp/src/test/java/com/example/thehiveapp/RilloSystemTest.java
@@ -0,0 +1,13 @@
+package com.example.thehiveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RilloSystemTest {
+
+    @Test
+//    @WithMockUser(username = "testUser", roles = {"USER"})
+    void contextLoads() {
+    }
+}
