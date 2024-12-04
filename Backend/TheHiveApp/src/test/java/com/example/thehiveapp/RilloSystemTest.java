d3957f061d7316622e93430115e0fdba6fa301d2
LucasNeroneRillo <lneronerillo@gmail.com>
2024-12-03 20:27:55 -0600

    72-backend-testing-lucas-rillo - Created basic test

--- Backend/TheHiveApp/src/test/java/com/example/thehiveapp/RilloSystemTest.java
+++ Backend/TheHiveApp/src/test/java/com/example/thehiveapp/RilloSystemTest.java
@@ -1,13 +1,42 @@
 package com.example.thehiveapp;
 
+import com.example.thehiveapp.dto.authentication.CompanySignUpRequest;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RilloSystemTest {
 
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+        RestAssured.baseURI = "http://localhost";
+    }
+
     @Test
-//    @WithMockUser(username = "testUser", roles = {"USER"})
-    void contextLoads() {
+    void signUpCompany() {
+        // Create a request object
+        CompanySignUpRequest companySignUpRequest = new CompanySignUpRequest();
+        companySignUpRequest.setEmail("test23r24r43f3lDkf@test.com");
+        companySignUpRequest.setPassword("PDassword234r@3#");
+        companySignUpRequest.setName("Test");
+
+        // Send the request and validate the response
+        Response response = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .body(companySignUpRequest)
+                .when()
+                .post("/account/signup/company");
+
+        assertEquals(200, response.getStatusCode());
     }
 }
